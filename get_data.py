724e5b567e78067901b433446d0dbed49ff743ad
Nathan Patnam <patnamnv@email.wofford.edu>
2018-07-23 14:48:43 -0700

    updated

--- get_data.py
+++ get_data.py
@@ -2,25 +2,26 @@
 import sys
 import requests
 import json
-def get_data(query =""):
-   api_key = "AIzaSyCYq06CBnnF27kRI_RNnhz3S0KoQPH1cNM"
-   search_engine_key = "&cx=005712099980169065800:ug7myqa_ep0"
-
-
-   query_string = "https://www.googleapis.com/customsearch/v1?key="
-   query_string+=api_key
-   query_string+=search_engine_key
-   query_string+= "&q=" + query
-
-   search_results = requests.get(query_string)
-   for item in _decode_response(search_results.content):
-       print (item)
 
 
 
+def get_results(query ="", tags=[]):
+    query_string = get_query_string(query, tags)
+    search_results = requests.get(query_string)
+    for item in decode_response(search_results.content):
+        print (item["title"])
 
 
-def _decode_response(json_string):
+def get_query_string(query = "", tags=[]):
+    api_key = "AIzaSyCYq06CBnnF27kRI_RNnhz3S0KoQPH1cNM"
+    search_engine_key = "&cx=005712099980169065800:ug7myqa_ep0"
+    query_string = "https://www.googleapis.com/customsearch/v1?key="
+    query_string+=api_key
+    query_string+=search_engine_key
+    query_string+= "&q=" + query
+    return query_string
+
+def decode_response(json_string):
     response = json.loads(json_string)
     meta = {key: value for key, value in response.items() if key != 'items'}
     num_results = int(meta['searchInformation']['totalResults'])
@@ -30,4 +31,6 @@ def _decode_response(json_string):
     for item in response['items']:
         item['meta'] = meta
     return response['items']
-get_data("pizza")
+
+
+get_results("unemployment during 2005 in USA")
